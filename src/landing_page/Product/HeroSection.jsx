e78a406af0c66130b59782019e4f764d411552a1
Vinayak Rastogi <vinayakr841@gmail.com>
2026-09-24 09:22:37 +0530

    Changes

--- src/landing_page/Product/HeroSection.jsx
+++ src/landing_page/Product/HeroSection.jsx
@@ -5,7 +5,7 @@ function HeroSection() {
   return (
     <div className="container-lg border-bottom pb-5 mb-5">
       <div className="row text-center pt-5 mt-5 pb-5">
-        <h3 style={{ color: "#444" }}>Zerodha Products</h3>
+        <h3 style={{ color: "#444" }}>Finora Products</h3>
         <p className="fs-5 mt-2 " style={{ color: "#444" }}>
           Sleek, modern, and intuitive trading platforms
         </p>
